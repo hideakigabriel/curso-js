acba47cb3126af39a413d624ec5cb5c56b3fd0a4
Gabriel Hideaki <114426202+hideakigabriel@users.noreply.github.com>
2023-08-31 22:47:53 -0300

    Add files via upload

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 28;
+
+if (idade < 16) {
+  console.log("Não vota");
+} else if (idade < 18) {
+  console.log("Voto opcional");
+} else if (idade >= 18) {
+  console.log('Voto obrigatório')
+}
